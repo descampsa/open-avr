b8efcee872a4e3c71ac3190a3dc47bc2b2198f94
Adrien Descamps <adrien.descamps@gmail.com>
2017-05-01 16:16:02 +0200

    update test software

--- software/prog.asm
+++ software/prog.asm
@@ -2,39 +2,12 @@
 .INCLUDE "tn11def.inc"
 .LIST
 
-.DEF	r = R16
-.DEF	ar1 = R17
-.DEF	ar2 = R18
+.DEF	r1 = R16
+.DEF	r2 = R17
 
-rjmp	main
-
-test_arithm:
-	ldi	ar1,0x02
-	ldi	ar2,0x04
-	add	ar1,ar2
-	cpi	ar1,0x06
-	brne	error
-	ret
-
-turn_off:
-	clr	r
-	out	PORTB,r
-	ret
-
-turn_on:
-	ser	r
-	out	PORTB,r
-	ret
-
-main:
-	ldi	r,0xFF
-	out	DDRB,r
-	rcall	test_arithm
-loop:
-	rcall	turn_off
-	rcall	turn_on
-	rjmp	loop
-
-error:
-	nop
-
+ldi	r1,0xFF
+ldi	r2,0x00
+out	DDRB,r1
+out PORTB,r1
+out PORTB,r2
+out DDRB,r2
