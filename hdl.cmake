991c202ff1706592fd93eca31377eec67c9844ea
Adrien Descamps <adrien.descamps@gmail.com>
2017-05-01 12:31:18 +0200

    add preprocessor variables for simulation/synthesis

--- hdl.cmake
+++ hdl.cmake
@@ -2,7 +2,7 @@
 function(add_simulation_target target)
 	get_property(dirs DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} PROPERTY INCLUDE_DIRECTORIES)
 	add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${target}.sim
-		COMMAND iverilog -I ${dirs} -o ${CMAKE_CURRENT_BINARY_DIR}/${target}.sim ${ARGN}
+		COMMAND iverilog -DSIMULATION=1 -I ${dirs} -o ${CMAKE_CURRENT_BINARY_DIR}/${target}.sim ${ARGN}
 		DEPENDS ${ARGN})
 	add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${target}.vcd
 		COMMAND vvp ${CMAKE_CURRENT_BINARY_DIR}/${target}.sim
@@ -15,7 +15,7 @@ endfunction(add_simulation_target)
 function(add_synthesis_target target pcf_file)
 	get_property(dirs DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} PROPERTY INCLUDE_DIRECTORIES)
 	add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${target}.blif
-		COMMAND yosys -q -p \"read_verilog -I${dirs} ${ARGN}\; synth_ice40 -blif ${CMAKE_CURRENT_BINARY_DIR}/${target}.blif\"
+		COMMAND yosys -q -p \"read_verilog -DICE40_SYNTHESIS=1 -I${dirs} ${ARGN}\; synth_ice40 -blif ${CMAKE_CURRENT_BINARY_DIR}/${target}.blif\"
 		DEPENDS ${ARGN})
 	add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${target}.pnr
 		COMMAND arachne-pnr -d 1k -P tq144 -p ${pcf_file} ${CMAKE_CURRENT_BINARY_DIR}/${target}.blif -o ${CMAKE_CURRENT_BINARY_DIR}/${target}.pnr
