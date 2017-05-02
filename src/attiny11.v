7585142b656fcaa940287a998cee58c92bf6383d
Adrien Descamps <adrien.descamps@gmail.com>
2017-05-02 22:27:51 +0200

    correctly handle rst in simulation

--- src/attiny11.v
+++ src/attiny11.v
@@ -17,6 +17,9 @@ module attiny11
 	avr_timer#(.IO_ADDR(50))
 		timer0(.clk(clk), .rst(~rst_inv), .io_addr(io_addr), .io_data(io_data), .io_read(io_read), .io_write(io_write), .T0(T0));
 	
+`ifdef SIMULATION
+	assign rst_inv = ~rst;
+`endif
 `ifdef ICE40_SYNTHESIS
 	SB_IO #(.PIN_TYPE(6'b000000), .PULLUP(1'b1)) 
 		rst_pin(.PACKAGE_PIN(rst), .D_IN_0(rst_inv), .INPUT_CLK(clk));
