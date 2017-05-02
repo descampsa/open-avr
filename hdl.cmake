74d38bb360640cbab446faeec7b7d65ebdc59f52
Adrien Descamps <adrien.descamps@gmail.com>
2017-05-02 22:22:23 +0200

    rename synthesis directory

--- hdl.cmake
+++ hdl.cmake
@@ -17,13 +17,13 @@ function(add_synthesis_target target pcf_file)
 	add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${target}.blif
 		COMMAND yosys -q -p \"read_verilog -DICE40_SYNTHESIS=1 -I${dirs} ${ARGN}\; synth_ice40 -blif ${CMAKE_CURRENT_BINARY_DIR}/${target}.blif\"
 		DEPENDS ${ARGN})
-	add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${target}.pnr
-		COMMAND arachne-pnr -d 1k -P tq144 -p ${pcf_file} ${CMAKE_CURRENT_BINARY_DIR}/${target}.blif -o ${CMAKE_CURRENT_BINARY_DIR}/${target}.pnr
-		COMMAND icetime -d hx1k -P tq144 ${CMAKE_CURRENT_BINARY_DIR}/${target}.pnr
+	add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${target}.asf
+		COMMAND arachne-pnr -d 1k -P tq144 -p ${pcf_file} ${CMAKE_CURRENT_BINARY_DIR}/${target}.blif -o ${CMAKE_CURRENT_BINARY_DIR}/${target}.asf
+		COMMAND icetime -d hx1k -P tq144 ${CMAKE_CURRENT_BINARY_DIR}/${target}.asf
 		DEPENDS ${pcf_file} ${CMAKE_CURRENT_BINARY_DIR}/${target}.blif)
 	add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${target}.bin
-		COMMAND icepack ${CMAKE_CURRENT_BINARY_DIR}/${target}.pnr ${CMAKE_CURRENT_BINARY_DIR}/${target}.bin
-		DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${target}.pnr)
+		COMMAND icepack ${CMAKE_CURRENT_BINARY_DIR}/${target}.asf ${CMAKE_CURRENT_BINARY_DIR}/${target}.bin
+		DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${target}.asf)
 	add_custom_target(${target}_synthesis
 		DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${target}.bin)
 	add_custom_target(${target}_upload
