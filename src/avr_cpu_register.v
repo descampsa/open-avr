f86037de6d11f289e3e0ec67dc7b4215cf74bb18
Adrien Descamps <adrien.descamps@gmail.com>
2017-05-02 22:13:39 +0200

    remove old commented code

--- src/avr_cpu_register.v
+++ src/avr_cpu_register.v
@@ -18,9 +18,7 @@ module avr_cpu_register
 		if(rst)
 			for(i=0; i<32; i=i+1)
 				register_bank[i] <= 8'b0;
-
-		//r_out <= register_bank[r_addr];
-		//d_out <= register_bank[d_addr];
-		register_bank[d_addr] <= d_in;
+		else
+			register_bank[d_addr] <= d_in;
 	end
 endmodule
